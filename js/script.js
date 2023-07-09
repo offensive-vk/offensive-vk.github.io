94f818cf67c456b64dcb1233383ea1a646ee3c99
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-07-09 12:00:15 +0530

    Fixes

--- js/script.js
+++ js/script.js
@@ -6,17 +6,17 @@ var text = document.querySelectorAll('p');
 var body = document.body;
 var isLightTheme = true;
 
-btn.addEventListener('click', function() {
+btn.addEventListener('click', function () {
     if (isLightTheme) {
         body.style.backgroundColor = "#fefefe";
         body.style.color = "";
-        text.forEach(function(p) {
+        text.forEach(function (p) {
             p.style.color = "#000";
         });
     } else {
         body.style.backgroundColor = "#000";
         body.style.color = "";
-        text.forEach(function(p) {
+        text.forEach(function (p) {
             p.style.color = "";
         });
     }
