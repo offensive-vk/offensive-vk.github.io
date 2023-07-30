6eed17ae6394b71b273e25d2862aab825b319a2a
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-07-30 13:44:28 +0530

    Major Changes as on Sunday - July, 30, 2023

--- js/script.js
+++ js/script.js
@@ -23,3 +23,52 @@ btn.addEventListener('click', function () {
 
     isLightTheme = !isLightTheme;
 });
+
+// Text Animations
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let interval = null;
+document.querySelector(".brand").onmouseover = event => {
+    let iteration = 0;
+    clearInterval(interval);
+    interval = setInterval(() => {
+        event.target.innerText = event.target.innerText
+            .split("")
+            .map((letter, index) => {
+                if (index < iteration) {
+                    return event.target.dataset.value[index];
+                }
+                return letters[Math.floor(Math.random() * 26)]
+            })
+            .join("");
+        if (iteration >= event.target.dataset.value.length) {
+            clearInterval(interval);
+        }
+        iteration += 1 / 3;
+    }, 30);
+}
+
+
+// Clock
+const display = () => {
+    let now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    let amOrPm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12 || 12;
+    if (hours < 10) {
+        hours = '0' + hours;
+    }
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    }
+    if(seconds < 10){
+        seconds = '0' + seconds;
+    }
+    const current = hours+" : "+minutes+" : "+seconds;
+    document.querySelector(".clock").innerHTML = current+" "+amOrPm;
+}
+const start = () => {
+    setInterval(display, 1000);
+}
+document.addEventListener("DOMContentLoaded", start);
