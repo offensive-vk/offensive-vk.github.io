0b19e1cde7278c6ab68c90534aba2330f1636a51
offensive-vk <121493131+offensive-vk@users.noreply.github.com>
2023-08-18 12:30:53 +0530

    New Menu Changes

--- js/script.js
+++ js/script.js
@@ -71,3 +71,19 @@ const start = () => {
     setInterval(display, 1000);
 }
 document.addEventListener("DOMContentLoaded", start);
+
+
+// Navbar- Links
+for (const eachLink of document.getElementsByClassName("link")) {
+    eachLink.onmousemove = (e) => {
+        const decimal = e.clientX / eachLink.offsetWidth;
+
+        const basePercent = 80,
+            percentRange = 20,
+            adjustablePercent = percentRange * decimal;
+
+        const lightBluePercent = basePercent + adjustablePercent;
+
+        eachLink.style.setProperty("--light-blue-percent", `${lightBluePercent}%`)
+    };
+}
