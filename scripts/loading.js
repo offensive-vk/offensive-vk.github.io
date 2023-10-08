7f23f8321e6656f5c274d0ec99943a246b416591
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-10-08 17:38:08 +0530

    Final Commits for this loader

--- scripts/loading.js
+++ scripts/loading.js
@@ -1,26 +1,36 @@
-
 /* Code for Progress bar on top */
 
-    // get the progress bar element
-    const progressBar = document.getElementById('progress-bar');
+const progressBar = document.getElementById('progress-bar');
 
-    // update the progress bar as the page loads
-    window.addEventListener('load', () => {
-        // document.getElementsById('progress-bar').style.width = '100%';
-        progressBar.style.width = '100%';
-        progressBar.classList.add('loading');
+// update the progress bar as the page loads
+window.addEventListener('load', () => {
+    // document.getElementsById('progress-bar').style.width = '100%';
+    progressBar.style.width = '100%';
+    progressBar.classList.add('loading');
 
-        // hide the progress bar after 2 seconds
-        setTimeout(() => {
-            progressBar.style.display = 'none';
-            progressBar.classList.remove('loading');
-        }, 3000);
-    });
+    // hide the progress bar after 3 seconds
+    setTimeout(() => {
+        progressBar.style.display = 'none';
+        progressBar.classList.remove('loading');
 
-    // update the progress bar as the user navigates the webpage
-    window.addEventListener('beforeunload', () => {
-        progressBar.style.width = '0%';
-        progressBar.style.display = 'block';
-        progressBar.classList.add('loading');
-    });
-	
+    }, 3000);
+});
+
+// update the progress bar as the user navigates the webpage
+window.addEventListener('beforeunload', () => {
+    progressBar.style.width = '0%';
+    progressBar.style.display = 'block';
+    progressBar.classList.add('loading');
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    const cssLink = document.getElementById("entry");
+    setTimeout(function() {
+        if (cssLink) {
+            cssLink.parentNode.removeChild(cssLink);
+        }
+        document.querySelector('#entry-loader').style.display = 'none';
+    }, 4200);
+});
+
+
