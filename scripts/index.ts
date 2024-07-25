45d830e97f2d795e57fb87d4fbf4d92503971998
Vedansh ✨ <data.offensive.gov@gmail.com>
2024-07-25 18:12:48 +0530

    Keys

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,4 +1,3 @@
-
 /**
  * @author: Vedansh Khandelwal
  * @description: Private Development and Testing Website.
@@ -7,21 +6,27 @@
  * @lang JavaScript (ES6+), TypeScript
  */
 class Methods {
-    private static PRIVATEKEY: number = 10859300155;
 
-    constructor() { }
+    private static PRIVATE_KEY: number = 10859300155;
+    private static PUBLIC_KEY: string = `0IF0VPIG842MP9VV`;
+    private constructor() { }
 
     public async getIP(): Promise<string> {
         try {
             const res = await fetch('https://www.ifconfig.me/ip');
             const ip = await res.text();
-            return "[ Your IP Address is : " + ip + " ]";
+            return ":: Your IP Address is : " + ip + " ::";
         } catch (error) {
-            return "[ Error fetching IP address ]";
+            return ":: Error fetching IP address ::";
         }
     }
+
     public async showKeys(key?: [number, string]): Promise<void> {
-        console.log(`Private Key : ${Methods.PRIVATEKEY}\n`)
+        console.log(`\t [INFO] Your New Private Key : ${Methods.PRIVATE_KEY}\n`)
+    }
+
+    public async showPubKeysReset(key?: string | number | unknown): Promise<void> {
+        console.log(`\t [INFO] Default Public Key : ${Methods.PUBLIC_KEY}\n`)
     }
 }
 
@@ -38,5 +43,6 @@ async function updateIP() {
         }
     }
 }
+
 setTimeout(updateIP, 2500);
 updateIP();
